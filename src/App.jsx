276f9a5cef137fa8ef1cb64d4f204f8883666fa6
Davi Cavalcante <davib506@gmail.com>
2025-02-09 19:24:13 -0300

    Develop: Adicionando font 'Poppins' globalmente

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className={`overflow-x-hidden flex flex-col`}>
+    <div className={`overflow-x-hidden flex flex-col font-main`}>
       <MenuProvider>
         <Header />
         <Outlet />
